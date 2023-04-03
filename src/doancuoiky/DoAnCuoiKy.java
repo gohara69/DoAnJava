27515b96d65246ce54f03fe257523ec252d84753
Minh Sang <ngos810@gmail.com>
2023-04-03 18:05:10 +0700

    seccond commit

--- src/doancuoiky/DoAnCuoiKy.java
+++ src/doancuoiky/DoAnCuoiKy.java
@@ -14,7 +14,7 @@ public class DoAnCuoiKy {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Hello World!!!
+        System.out.println("Hello World!!!");
     }
     
 }
