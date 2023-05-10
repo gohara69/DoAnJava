c0228807dd4c6018b98b032a4d27eb91ed32aebd
Minh Sang <ngos810@gmail.com>
2023-05-10 18:43:07 +0100

    Sang lam chuc nang dat mon them va chon ban

--- src/DAO/MonDAO.java
+++ src/DAO/MonDAO.java
@@ -38,4 +38,25 @@ public class MonDAO {
         } 
         return ds_mon;
     }
+    public static ArrayList<Mon> layDanhSachMonAnTheoDM(int danhMuc) {
+        ArrayList<Mon> ds_mon = new ArrayList<Mon>();
+        try {
+            String sql = "Select * from MON where DM_ID = " + danhMuc;
+            DataService ds = new DataService();
+            ds.open();
+            ResultSet rs = ds.executeQuery(sql);
+            while(rs.next()) {
+                Mon mon = new Mon();
+                mon.setDanhMuc(rs.getInt("DM_ID"));
+                mon.setTenMon(rs.getString("M_TEN"));
+                mon.setGiaMon(rs.getInt("M_GIA"));
+                mon.setSrcAnh(rs.getString("M_IMG"));
+                
+                ds_mon.add(mon);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(MonDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        return ds_mon;
+    }
 }
